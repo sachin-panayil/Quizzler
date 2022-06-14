58fd309b7e9408d10f2d282012be3d3b78934d5b
Sachin Panayil <sachinpanayil01@gmail.com>
2022-06-13 23:02:36 -0400

    finished project

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  Quizzler-iOS13
+//
+//  Created by Sachin Panayil on 6/13/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+    
+    
+}
